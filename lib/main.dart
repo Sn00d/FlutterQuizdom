906625edd70701b0e19af2bf5d5e5881c1c47aa2
Georgios Fotopoulos <fotopgeorgios@gmail.com>
2018-11-18 11:33:50 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quizdom/pages/landing_page.dart';
+import 'package:quizdom/pages/quiz_page.dart';
+import 'pages/score_page.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new LandingPage(),
+  ));
+}
